75bea050741e053e3bce77eb8d77441055905e57
swiftty <62803132+swiftty@users.noreply.github.com>
2022-06-29 02:55:36 +0900

    add Caching protocol

---
+++ Sources/DataCacheKit/Caching.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public protocol Caching<Key, Value> {
+    associatedtype Key: Hashable & Sendable
+    associatedtype Value
+
+    subscript (key: Key) -> Value? { get async throws }
+
+    func value(for key: Key) async throws -> Value?
+
+    @discardableResult
+    func store(_ value: Value, for key: Key) -> Task<Void, Never>
+
+    @discardableResult
+    func remove(for key: Key) -> Task<Void, Never>
+
+    @discardableResult
+    func removeAll() -> Task<Void, Never>
+}
