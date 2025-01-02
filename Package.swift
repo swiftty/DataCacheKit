c953998d34f0fa75c57725fc7a076960867a291b
swiftty <62803132+swiftty@users.noreply.github.com>
2025-01-02 10:54:55 +0900

    Update MemoryCache implementation

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "DataCacheKit",
     platforms: [
-        .iOS(.v14),
-        .tvOS(.v14),
+        .iOS(.v16),
+        .tvOS(.v16),
         .watchOS(.v8),
-        .macOS(.v12)
+        .macOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
